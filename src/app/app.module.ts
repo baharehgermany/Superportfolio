8e20bd54b2e3684833db63a59aa536a22c14c293
Bahareh Etemadi <baharehetemadi81@gmail.com>
2022-01-18 13:40:03 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { IconsComponent } from './icons/icons.component';
+import { WorkComponent } from './work/work.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { EmailComponent } from './email/email.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    IconsComponent,
+    WorkComponent,
+    ProjectsComponent,
+    AboutComponent,
+    EmailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
